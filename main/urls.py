0db7cd73db30a2667fc8d72c5ab608f8226f9f5d
itqonibras <itqonnibras@gmail.com>
2023-09-27 14:02:08 +0700

    Tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import login_user, logout_user, show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import edit_product, login_user, logout_user, register, delete_product
 
 app_name = 'main'
 
@@ -13,4 +14,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>/', edit_product, name='edit_product'),
+    path('delete-product/<int:id>/', delete_product, name='delete_product'),
+
 ]
